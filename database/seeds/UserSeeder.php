019be54e27462b9aa660070161331525a7ef5e50
Émilio Gonzalez <egg997@gmail.com>
2017-08-12 16:58:13 -0400

    - Added profile pics to the user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,18 +11,39 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+	    $imageWidth = 200;
 	    DB::table('users')->insert([
-		    'name' => 'Alexandre',
-		    'password' => bcrypt('Alexandre'),
-		    'email' => 'Alexandre@exemple.com',
+		    'name' => 'Phoenix Chabot',
+		    'password' => bcrypt('phoenix'),
+		    'email' => 'Phoenix@exemple.com',
+		    'profilePicUrl' => 'https://graph.facebook.com/v2.10/100000400011054/picture?type=square&width=' . $imageWidth,
 		    'created_at' => Carbon\Carbon::create(),
 		    'updated_at' => Carbon\Carbon::create()
 	    ]);
 
 	    DB::table('users')->insert([
-		    'name' => 'Gonzo',
-		    'password' => bcrypt('Gonzo'),
-		    'email' => 'Gonzo@exemple.com',
+		    'name' => 'Émilio Gonzalez',
+		    'password' => bcrypt('Emilio'),
+		    'email' => 'Emilio@exemple.com',
+		    'profilePicUrl' => 'https://graph.facebook.com/v2.10/100001798174533/picture?type=square&width=' . $imageWidth,
+		    'created_at' => Carbon\Carbon::create(),
+		    'updated_at' => Carbon\Carbon::create()
+	    ]);
+
+	    DB::table('users')->insert([
+		    'name' => 'Monica Martin',
+		    'password' => bcrypt('Monica'),
+		    'email' => 'Monica@exemple.com',
+		    'profilePicUrl' => 'https://graph.facebook.com/v2.10/100000075486269/picture?type=square&width=' . $imageWidth,
+		    'created_at' => Carbon\Carbon::create(),
+		    'updated_at' => Carbon\Carbon::create()
+	    ]);
+
+	    DB::table('users')->insert([
+		    'name' => 'Jérémy Fortier',
+		    'password' => bcrypt('Jeremy'),
+		    'email' => 'Jeremy@exemple.com',
+		    'profilePicUrl' => 'https://graph.facebook.com/v2.10/100001919068365/picture?type=square&width=' . $imageWidth,
 		    'created_at' => Carbon\Carbon::create(),
 		    'updated_at' => Carbon\Carbon::create()
 	    ]);
